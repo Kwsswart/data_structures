b8887159cd1b25286a5f308155d60cca3014d0e6
Kieron Spearing <kieron@stylesage.co>
2022-06-16 22:00:22 +0200

    Add Priority Queue

--- data_structures/queue.py
+++ data_structures/queue.py
@@ -1,5 +1,9 @@
 """
 Implementations of different types of queues.
+
+SimpleQueue - Standard implementation of First In First Out Queue.
+CircularQueue - Implementation of a circular queue where the last element points to the first, linking it.
+PriorityQueue - Implementation of a Priority Queue using the max-heap implementation.
 """
 from abc import ABC, abstractmethod
 
@@ -106,3 +110,98 @@ class CircularQueue(Queue):
         if self.is_empty():
             return None
         return self.queue[self.front]
+
+
+class PriorityQueue(Queue):
+    FRONT_OF_QUEUE_INDEX = 1
+
+    def __init__(self, queue_size):
+        self.max_queue_size = queue_size
+        self.current_queue_size = 0
+        self.queue = [None] * (queue_size + 1)
+
+    def heapify(self, index):
+        # Check that it is not the root node.
+        if not self.check_if_leaf_node(index):
+
+            # Check to see if the root is smaller than any of it's children
+            if self.queue[index] < self.queue[self.get_left_child_index(index)] or \
+                    self.queue[index] < self.queue[self.get_right_child_index(index)]:
+
+                # if the root node is smaller than either child, check which child is the largest and swap the
+                # root node with that node.
+                if self.queue[self.get_left_child_index(index)] > self.queue[self.get_right_child_index(index)]:
+                    self.swap_nodes(index, self.get_left_child_index(index))
+                    self.heapify(self.get_left_child_index(index))
+
+                else:
+                    self.swap_nodes(index, self.get_right_child_index(index))
+                    self.heapify(self.get_right_child_index(index))
+
+    def enqueue(self, item):
+
+        if self.is_full():
+            return None
+
+        self.current_queue_size += 1
+        self.queue[self.current_queue_size] = item
+
+        current_index = self.current_queue_size
+
+        while self.queue[current_index] > self.queue[self.get_parent_index(current_index)]:
+            self.swap_nodes(current_index, self.get_parent_index(current_index))
+            current_index = self.get_parent_index(current_index)
+
+    def dequeue(self):
+        if self.is_empty():
+            return None
+
+        value = self.queue[self.FRONT_OF_QUEUE_INDEX]
+
+        self.queue[self.FRONT_OF_QUEUE_INDEX] = self.queue[self.current_queue_size]
+        self.current_queue_size -= 1
+        self.heapify(self.FRONT_OF_QUEUE_INDEX)
+
+        return value
+
+    def is_empty(self):
+        return all(item is None for item in self.queue)
+
+    def is_full(self):
+        return self.current_queue_size >= self.max_queue_size
+
+    def peek(self):
+        return self.queue[self.FRONT_OF_QUEUE_INDEX]
+
+    def check_if_leaf_node(self, index):
+        if (self.current_queue_size // 2) <= index <= self.current_queue_size:
+            return True
+        return False
+
+    def swap_nodes(self, root_node, child_node):
+        """
+        If the root node is smaller than either child, check which child is the largest and swap the root node with that
+         node. This way the root will always be the highest element (lowest index) in the list.
+        """
+        self.queue[root_node], self.queue[child_node] = self.queue[child_node], self.queue[root_node]
+
+    @staticmethod
+    def get_parent_index(index):
+        """
+        Return the index of the parent for the node at the index
+        """
+        return index // 2
+
+    @staticmethod
+    def get_left_child_index(index):
+        """
+        Return the index of the left child for the node at the index
+        """
+        return 2 * index
+
+    @staticmethod
+    def get_right_child_index(index):
+        """
+        Return the index of the right child for the node at the index
+        """
+        return (2 * index) + 1
