1d0a1428c368243c2bdd745b03ac5ae7b30b0f9a
Kieron Spearing <kieron@stylesage.co>
2022-06-16 16:30:34 +0200

    Stack

---
+++ stack.py
@@ -0,0 +1,28 @@
+"""
+Implementation of the Stack Data Structure.
+"""
+
+
+class Stack:
+
+    def __init__(self):
+        self.stack = []
+
+    def is_empty(self):
+        return self.stack == []
+
+    def push(self, item):
+        self.stack.append(item)
+
+    def pop(self):
+        if self.is_empty():
+            return "Stack is empty"
+        return self.stack.pop()
+
+    def peek(self):
+        if self.is_empty():
+            return "Stack is empty"
+        return self.stack[-1]
+
+    def get_stack(self):
+        return self.stack
